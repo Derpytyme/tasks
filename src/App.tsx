28056825b9f27d8e4254d826bb436e11a258e03c
Alexander D <alexander.drolet@maine.edu>
2025-04-12 16:45:35 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): React.JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Xander is cool and epic also
+                automatically reload. Xander is cool and epic also. Hello World
             </p>
         </div>
     );
